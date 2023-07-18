66a9101b43207b152239cf307e4c9bb21807d268
Sergey Fomenko <artem.work749@gmail.com>
2023-07-18 16:02:39 +0300

    Indexes

--- advanced_level/code.sql
+++ advanced_level/code.sql
@@ -39,3 +39,35 @@ FROM ticket t
          JOIN flight f on f.id = t.flight_id
 WHERE f.flight_no = 'MN3002'
   AND f.departure_date::DATE = '2020-06-14';
+
+
+
+  -- Indexes
+SELECT * FROM ticket
+WHERE id = 5;
+
+CREATE UNIQUE INDEX unique_flight_id_seat_no_idx ON ticket (flight_id, seat_no);
+
+SELECT *
+FROM ticket
+WHERE flight_id = 5;        --  скорее-всего будет искать при помощи индекса. Это зависит от оптимизатора, количество записей и селективности
+
+SELECT *
+FROM ticket
+WHERE seat_no = 'B1';       -- будет full scan, так как индекс начинается не с seat_no
+
+
+SELECT *
+FROM ticket
+WHERE flight_id = 5
+    and seat_no = 'B1';     --будет использоваться индекс. Здесь seat_no может идти в начале условия
+
+
+
+select count(distinct flight_id) from ticket;   --9
+select count(*) from ticket;        --55
+--Селективностью flight_id будет 9/55. Это плохая селективность
+--Селективность - отношение количества строк, содержащих уникальную запись к общему количеству строк
+--При комбинированном flight_id + seat_no индекс будет уникальным. Это значит селективность = 55/55. Это хорошая селективность
+
+-- Много индексов на одну таблицу - плохо. При обновлении таблицы каждый раз будут обновляться индексы. А это другие файлы.
